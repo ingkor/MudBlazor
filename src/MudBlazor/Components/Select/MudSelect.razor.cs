a425088b256242966d75938a2d7468fc2140c274
Jakob <jksg@fwo.be>
2023-03-15 19:02:49 +0100

    first commit

--- src/MudBlazor/Components/Select/MudSelect.razor.cs
+++ src/MudBlazor/Components/Select/MudSelect.razor.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
